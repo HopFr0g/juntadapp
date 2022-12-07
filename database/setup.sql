b0a19cef65637cb0377153f5ced0e9c851411ecc
HopFr0g <hopfr0g@outlook.com>
2022-12-06 22:19:36 -0300

    Creacion de Plantilla 4

--- database/setup.sql
+++ database/setup.sql
@@ -11,6 +11,7 @@ create table reunion(
     hash            varchar(32)     not null,
     nombre          varchar(64)     not null,
     descripcion     varchar(120)    null,
+    fecha_creacion  text            not null                default current_timestamp,
     foreign key(id_ip) references ip(id),
     unique(hash)
 );
@@ -19,6 +20,7 @@ create table persona(
     id              integer         primary key,
     id_reunion      integer         not null,
     nombre          varchar(32)     not null,
+    fecha_creacion  text            not null                default current_timestamp,
     foreign key(id_reunion) references reunion(id),
     unique(nombre)
 );
@@ -27,7 +29,7 @@ create table persona_ip(
     id              integer         primary key,
     id_persona      integer         not null,
     id_ip           integer         not null,
-    fecha           text            not null,
+    fecha           text            not null                default current_timestamp,
     foreign key(id_persona) references persona(id),
     foreign key(id_ip) references ip(id),
     unique(id_persona, id_ip, fecha)
