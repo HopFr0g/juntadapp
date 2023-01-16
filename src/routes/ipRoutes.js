56d6dca9e7a25d772e208fc6e674905565a53a2f
HopFr0g <hopfr0g@outlook.com>
2023-01-15 21:54:02 -0300

    Swagger: Documentacion terminada

--- src/routes/ipRoutes.js
+++ src/routes/ipRoutes.js
@@ -3,6 +3,16 @@ const ipController = require("../controllers/ipController.js");
 const express = require("express");
 const router = express.Router();
 
+/**
+ * @openapi
+ * /api/ip:
+ *   get:
+ *     tags:
+ *       - Ip
+ *     responses:
+ *       default:
+ *         $ref: '#/components/responses/default'
+ */
 router.get("/", ipController.findAll);
 
 module.exports = router;
--- src/routes/personaRoutes.js
+++ src/routes/personaRoutes.js
@@ -3,7 +3,39 @@ const personaController = require("../controllers/personaController.js");
 const express = require("express");
 const router = express.Router();
 
+/**
+ * @openapi
+ * /api/persona/{hash}:
+ *   get:
+ *     tags:
+ *       - Persona
+ *     parameters:
+ *       - in: path
+ *         name: hash
+ *         schema:
+ *           type: string
+ *     responses:
+ *       default:
+ *         $ref: '#/components/responses/default'
+ */
 router.get("/:hash", personaController.findAllByHash);
+
+/**
+ * @openapi
+ * /api/persona:
+ *   post:
+ *     tags:
+ *       - Persona
+ *     requestBody:
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             $ref: '#/components/schemas/Persona'
+ *     responses:
+ *       default:
+ *         $ref: '#/components/responses/default'
+ */
 router.post("/", personaController.create);
 
 module.exports = router;
